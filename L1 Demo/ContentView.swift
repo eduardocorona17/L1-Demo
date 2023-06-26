24a0f45f459ff7b50752bc86720bb574fad1b843
Eduardo Corona <ecoronalopez@gmail.com>
2023-06-25 17:20:20 -0700

    Finished Basics.

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -11,21 +11,70 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color(.black)
+            Color(.white)
                 .ignoresSafeArea()
             
-            VStack {
-            
-                Image("iOSDemo")
-                    .resizable()
-                    .cornerRadius(5)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                Text("Valley Sunset")
-                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color(hue: 0.093, saturation: 0.731, brightness: 1.0))
+            ZStack {
+                Color(.systemMint)
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading, spacing: 20.0) {
+                    
+                    Image("iOSDemo")
+                        .resizable()
+                        .cornerRadius(15)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                    
+                    HStack {
+                        
+                        Text("Valley Sunset")
+                            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(hue: 0.093, saturation: 0.731, brightness: 1.0))
+                        
+                        Spacer()
+                        
+                        VStack {
+                            
+                            HStack {
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.leadinghalf.filled")
+                            }
+                            
+                            Text("Reviews 222")
+                        }
+                        .foregroundColor(.cyan)
+                        .font(.caption)
+                        
+                    }
+                    
+                    
+                    
+                    Text("Come visit Napa Valley.")
+                        .font(.title)
+                        .fontWeight(.light)
+                        .foregroundColor(Color(hue: 0.09, saturation: 0.731, brightness: 0.98))
+                    
+                    HStack {
+                        Spacer()
+                        Image(systemName: "fork.knife")
+                        Image(systemName: "binoculars.fill")
+                    }
+                    .foregroundColor(.cyan)
+                    .font(.caption)
+                }
+                .padding()
+                .background(Rectangle().foregroundColor(Color(hue: 0.479, saturation: 1.0, brightness: 0.538))
+                    .cornerRadius(15)
+                    .shadow(radius: 15))
+                
             }
+            
+            
         }
 
        
